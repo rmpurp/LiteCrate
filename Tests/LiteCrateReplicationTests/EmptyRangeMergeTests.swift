bf355b453bae8e50009fa2559f762502defcbe4d
Ryan Purpura <ryan@rpurp.com>
2022-06-28 00:53:33 -0700

    WIP EmptyRangeMergeTests

---
+++ Tests/LiteCrateReplicationTests/EmptyRangeMergeTests.swift
@@ -0,0 +1,24 @@
+//
+//  OverallTests.swift
+//
+//
+//  Created by Ryan Purpura on 6/27/22.
+//
+
+@testable import LiteCrate
+@testable import LiteCrateReplication
+import XCTest
+
+fileprivate struct Model {
+  let modelName: String
+}
+
+// TODO: Test node id tie-breaking
+final class EmptyRangeMergeTests: XCTestCase {
+  func testMergeRanges() throws {
+    let c
+  }
+  
+  func testMerge() throws {
+  }
+}
